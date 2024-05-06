724a4dd3ea7feef9eb79e453a032e4e5b67748ce
jkipping <79582668+jkipping@users.noreply.github.com>
2024-05-06 11:59:59 +0200

    nieuwe poging 1e setup constraints & decision variables

--- Operations.py
+++ Operations.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
+from gurobipy import Model,GRB,LinExpr,quicksum
 
+## Initiate Gurobi model
+m = Model()
 tab1 = pd.read_csv('tab1.csv', sep=';')
 tab2 = pd.read_csv('tab2.csv', sep=';')
 
@@ -25,4 +28,42 @@ N_a_fi = tab1['Number of arr. passengers'] #number of arrival passengers of flig
 N_d_fi = tab1['Number of dep. passengers'] #number of departure passengers of flight f_i
 N_m_fi = tab1['Number of transit passengers'] #number of transit passengers of flight f_i
 
+## Decision variables
+for i in F.keys():
+    for k in G.keys():
+        y[i,k] = m.addVar(lb=0, ub=1,
+                                vtype=GRB.BINARY,
+                                name='y[%s,%s]'%(i,k))
+for i in F.keys():
+    for j in F.keys():
+        z[i,j] = m.addVar(lb=0, ub=1,
+                                    vtype=GRB.BINARY,
+                                    name='z[%s,%s]'%(i,j))
 
+m.update()
+
+## Constraints
+#C1 - 80% aerobridge
+C1 = m.addConstrs(((((quicksum(quicksum( for k in K.keys())... for .. in ...)/() >= 0.8)
+                    for j in Delta_minus_jk[(i,k)]) == 1)
+                    for i in C.keys()),name='C1')
+
+#C2 - each flight is assigned to exactly 1 gate
+C2 = m.addConstrs(((quicksum(y[i,k] == 1 for k in G.keys())
+                    for i in C.keys()),name='C2'))
+
+#C3 - y is binary (in decision variables)
+
+#C4 - z_fi,fj = 1 if fi and fj assigned to same gate
+C4 = m.addConstrs(((quicksum(quicksum(quicksum(for k in G.keys()) for j>i, fj in F.keys())for i in F.keys()))
+                    , name = 'C4'))
+
+#C5 - safety interval if assigned to same gate
+C5 = m.addConstrs(((a_fi - d_fi + (1-z[i,j])*M >= T)
+                  for i in F.keys()
+                  for j in F.keys() if i<j), name = 'C5')
+
+#C6 - gate type meets AC type
+C6 = m.addConstrs((c_fi <= (c_g + (1-y[i,k])*M)
+                   for i in F.Keys()
+                   for k in G.keys()), name = 'C6')
