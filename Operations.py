24e036b4aa4010579833026b99212c4ca7e44464
Bram <brambuijvoets@gmail.com>
2024-05-06 11:50:14 +0200

    worked out symbol definition

--- Operations.py
+++ Operations.py
@@ -1,23 +1,28 @@
 import numpy as np
 import pandas as pd
 
+tab1 = pd.read_csv('tab1.csv', sep=';')
+tab2 = pd.read_csv('tab2.csv', sep=';')
 
-## symbol definition
-F = # flight set
-c_fi = #size of aircraft which executes flight f_i
-L = #set of airlines
-F_la = #flight set of airline a
-G = #Gate set
 
-c_g = #size of gate g_k
-a_fi = # arrival time of flight f_i
-d_fi = # departure time of flight f_i
-T = #minimum time interval of two flight which are assigned to the same gate [min]
-S_a_gk = #distance of arrival passenger walkingfrome gate g_k to baggage hall
-S_d_gk = #distance of departure passenger walking from security to gate g_k
-S_m_gk = #distance between gate g_k and transit counter
-N_a_fi = #number of arrival passengers of flight f_i
-N_d_fi = #number of departure passengers of flight f_i
-N_m_fi = #number of transit passengers of flight f_i
+### symbol definition
+F = tab1['Flight no.'] # flight set
+c_fi = tab1['Type'] #size of aircraft which executes flight f_i
+L = tab1['Airline'].drop_duplicates() #set of airlines
+F_L = {} #TODO: change this in constraints
+for airline in L:
+    F_L[airline] = tab1.loc[tab1['Airline'] == airline, 'Flight no.']
+G = tab2['Gate no.']#Gate set
+
+c_g = tab2['Gate size'] #size of gate g_k
+a_fi = tab1['Arr. time'] # arrival time of flight f_i
+d_fi = tab1['Dep. time'] # departure time of flight f_i
+T = 15 #minimum time interval of two flight which are assigned to the same gate [min]
+S_a_gk = tab2['Distance to the baggage hall (unit: m)'] #distance of arrival passenger walkingfrome gate g_k to baggage hall
+S_d_gk = tab2['Distance to the security check points (unit: m)'] #distance of departure passenger walking from security to gate g_k
+S_m_gk = tab2['Distance to the transit counter (unit: m)'] #distance between gate g_k and transit counter
+N_a_fi = tab1['Number of arr. passengers'] #number of arrival passengers of flight f_i
+N_d_fi = tab1['Number of dep. passengers'] #number of departure passengers of flight f_i
+N_m_fi = tab1['Number of transit passengers'] #number of transit passengers of flight f_i
 
 
