ee58698ff4ca7a020e1cc52ce119a76ceff19823
Bram <brambuijvoets@gmail.com>
2024-05-06 11:03:56 +0200

    created Operations.py

---
+++ Operations.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
+## symbol definition
+F = # flight set
+c_fi = #size of aircraft which executes flight f_i
+L = #set of airlines
+F_la = #flight set of airline a
+G = #Gate set
+
+c_g = #size of gate g_k
+a_fi = # arrival time of flight f_i
+d_fi = # departure time of flight f_i
+T = #minimum time interval of two flight which are assigned to the same gate [min]
+S_a_gk = #distance of arrival passenger walkingfrome gate g_k to baggage hall
+S_d_gk = #distance of departure passenger walking from security to gate g_k
+S_m_gk = #distance between gate g_k and transit counter
+N_a_fi = #number of arrival passengers of flight f_i
+N_d_fi = #number of departure passengers of flight f_i
+N_m_fi = #number of transit passengers of flight f_i
+
+
